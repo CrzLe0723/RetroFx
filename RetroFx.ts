683ab9478e3b9d9eddd9f6f67cd2a4ca2e7e2ea2
CrzLe0723 <Cruzle0723@gmail.com>
2026-04-03 20:08:54 -0700

    Update RetroFx.ts

--- RetroFx.ts
+++ RetroFx.ts
@@ -88,7 +88,7 @@ namespace Retro {
             //% callbacks.shadow=lists_create_with
             //% x.defl=80 y.defl=60
             //% dimensionX.defl=80 dimensionY.defl=60
-            //% help=github:Retro Fx+ Extension/docs/create-menu
+            //% help=github:Retro Fx/docs/create-menu
             static createMenu(options: string[], callbacks: (() => void)[], x: number, y: number, dimensionX: number, dimensionY: number, title: string) {
                 return new Menu(options, callbacks, x, y, dimensionX, dimensionY, title)
             }
